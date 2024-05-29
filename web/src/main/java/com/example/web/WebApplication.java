90bfd9749e61ef982fc03bf018ccf1a32e2c77e3
byul <bbyyuull33@gmail.com>
2024-05-29 22:53:05 +0900

    rooms, group_room_details 테이블 entity, repository 추가

--- web/src/main/java/com/example/web/WebApplication.java
+++ web/src/main/java/com/example/web/WebApplication.java
@@ -2,8 +2,10 @@ package com.example.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WebApplication {
 
 	public static void main(String[] args) {
---
+++ web/src/main/java/com/example/web/domain/GroupRoomDetail.java
@@ -0,0 +1,55 @@
+package com.example.web.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "group_room_details")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GroupRoomDetail {
+
+    @Id
+    private Integer roomId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "room_id")
+    @JsonBackReference
+    @JsonIgnoreProperties
+    private Room room;
+
+    @Setter
+    @Column(nullable = false)
+    private String roomName;
+
+    @Setter
+    @Column(nullable = false)
+    private Integer roomOwner;
+
+    @Setter
+    @Column(nullable = false)
+    private String enterCode;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public GroupRoomDetail(Room room) {
+        this.room = room;
+    }
+}
