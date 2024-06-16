123b3304878af60f11cbcb9af2017a4865bbdab1
ByulKim <bbyyuull33@gmail.com>
2024-06-16 14:31:06 +0900

    VO 클래스 equals, hashcode 메서드 lombok 어노테이션으로 대체

--- web/src/main/java/com/example/web/vo/GroupRoomDetailVo.java
+++ web/src/main/java/com/example/web/vo/GroupRoomDetailVo.java
@@ -1,26 +1,15 @@
 package com.example.web.vo;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import java.util.Objects;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class GroupRoomDetailVo {
     private String roomName;
     private Integer roomOwnerId;
     private String enterCode;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupRoomDetailVo that = (GroupRoomDetailVo) o;
-        return Objects.equals(roomName, that.roomName) && Objects.equals(roomOwnerId, that.roomOwnerId) && Objects.equals(enterCode, that.enterCode);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(roomName, roomOwnerId, enterCode);
-    }
 }
