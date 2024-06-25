ba02acab51b6a872e072dba38d015cf0ddfcdff5
ByulKim <bbyyuull33@gmail.com>
2024-06-25 18:10:55 +0900

    공통 모듈(common) 생성 및 설정

--- web/src/main/java/com/example/web/WebApplication.java
+++ web/src/main/java/com/example/web/WebApplication.java
@@ -1,11 +1,14 @@
 package com.example.web;
 
+import com.example.common.PropertyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Import(PropertyConfig.class)
 public class WebApplication {
 
 	public static void main(String[] args) {
