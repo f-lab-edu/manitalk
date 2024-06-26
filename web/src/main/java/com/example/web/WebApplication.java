64214fea32b44a48c946d58815c0c6b9148cbb75
ByulKim <bbyyuull33@gmail.com>
2024-06-26 13:54:36 +0900

    웹서버 mongodb 설정 및 엔티티 클래스, 레포지토리 추가

--- web/src/main/java/com/example/web/WebApplication.java
+++ web/src/main/java/com/example/web/WebApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableMongoAuditing
 @Import(PropertyConfig.class)
 public class WebApplication {
 
