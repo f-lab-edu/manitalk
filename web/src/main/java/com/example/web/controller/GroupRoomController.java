3b48c03d24553b093892f054c13b21d82563d12c
ByulKim <bbyyuull33@gmail.com>
2024-07-24 14:11:23 +0900

    Swagger - API 설명 추가

--- web/src/main/java/com/example/web/controller/GroupRoomController.java
+++ web/src/main/java/com/example/web/controller/GroupRoomController.java
@@ -2,6 +2,8 @@ package com.example.web.controller;
 
 import com.example.web.dto.*;
 import com.example.web.service.GroupRoomService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,20 +13,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/group/room")
 @RequiredArgsConstructor
+@Tag(name = "Group Room", description = "그룹 채팅방 API")
 public class GroupRoomController {
 
     private final GroupRoomService groupRoomService;
 
+    @Operation(summary = "그룹 채팅방 생성")
     @PostMapping
     public ResponseEntity<CreateGroupRoomResponse> createGroupRoom(@Valid @RequestBody CreateGroupRoomRequest dto) {
         return new ResponseEntity<>(groupRoomService.createGroupRoom(dto), HttpStatus.OK);
     }
 
+    @Operation(summary = "그룹 채팅방 입장")
     @PostMapping("/enter")
     public ResponseEntity<EnterRoomResponse> enterGroupRoom(@Valid @RequestBody EnterGroupRoomRequest dto) {
         return new ResponseEntity<>(groupRoomService.enterGroupRoom(dto), HttpStatus.OK);
     }
 
+    @Operation(summary = "그룹 채팅방 종료")
     @DeleteMapping
     public ResponseEntity<EndRoomResponse> endGroupRoom(@Valid @RequestBody EndGroupRoomRequest dto) {
         return new ResponseEntity<>(groupRoomService.endGroupRoom(dto), HttpStatus.OK);
--- web/src/main/java/com/example/web/controller/ManitoRoomController.java
+++ web/src/main/java/com/example/web/controller/ManitoRoomController.java
@@ -2,6 +2,8 @@ package com.example.web.controller;
 
 import com.example.web.dto.*;
 import com.example.web.service.ManitoRoomService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,15 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/manito/room")
 @RequiredArgsConstructor
+@Tag(name = "Manito Room", description = "마니또 채팅방 API")
 public class ManitoRoomController {
 
     private final ManitoRoomService manitoRoomService;
 
+    @Operation(summary = "마니또 채팅방 생성")
     @PostMapping
     public ResponseEntity<CreateManitoRoomResponse> createManitoRooms(@Valid @RequestBody CreateManitoRoomRequest dto) {
         return new ResponseEntity<>(manitoRoomService.createManitoRooms(dto), HttpStatus.OK);
     }
 
+    @Operation(summary = "마니또 채팅방 입장")
     @PostMapping("/enter")
     public ResponseEntity<EnterManitoRoomResponse> enterManitoRoom(@Valid @RequestBody EnterManitoRoomRequest dto) {
         return new ResponseEntity<>(manitoRoomService.enterManitoRoom(dto), HttpStatus.OK);
