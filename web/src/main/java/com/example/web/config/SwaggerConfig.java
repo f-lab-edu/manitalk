108d504b2ce0a633e9ff7d4404f588dedef3e778
May <31975535+ByulKim@users.noreply.github.com>
2024-07-26 17:57:10 +0900

    [#29] API 문서화 - Swagger (#30)
    * Swagger API 문서 적용
    
    * Swagger - API 설명 추가

---
+++ web/src/main/java/com/example/web/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.web.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Manitalk API Documentation")
+                .description("마니톡 API 문서")
+                .version("1.0.0");
+    }
+}
