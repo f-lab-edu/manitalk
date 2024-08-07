58f463238cda0f34b0a00b3e060336f113a3aceb
ByulKim <bbyyuull33@gmail.com>
2024-08-07 21:06:13 +0900

    Naver Oauth2 적용 - 클라이언트 테스트 페이지 수정

---
+++ websocket/src/main/java/com/example/websocket/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.websocket.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @Value("${auth.url}")
+    private String authUrl;
+
+    @Value("${logout.url}")
+    private String logoutUrl;
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("authUrl", authUrl);
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("logoutUrl", logoutUrl);
+        return "home";
+    }
+}
