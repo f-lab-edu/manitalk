3bf7815723b775badba4d1742230dd3c7faa71da
ByulKim <bbyyuull33@gmail.com>
2024-08-07 21:05:19 +0900

    Naver Oauth2 적용 - 네이버 아이디로 로그인

---
+++ web/src/main/java/com/example/web/config/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.example.web.config;
+
+import com.example.web.service.auth.CustomAuthenticationSuccessHandler;
+import com.example.web.service.auth.CustomLogoutSuccessHandler;
+import com.example.web.service.auth.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${client.url}")
+    private String clientUrl;
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
+
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(List.of("*"));
+            config.setAllowedMethods(List.of("*"));
+            config.setAllowedOriginPatterns(List.of(clientUrl));
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/**", "/login", "/oauth2/**", "/manitalk-api-docs","/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/login")
+                                .successHandler(customAuthenticationSuccessHandler)
+                                .userInfoEndpoint(userInfoEndpoint ->
+                                        userInfoEndpoint
+                                                .userService(customOAuth2UserService)
+                                )
+                )
+                .logout(logout ->
+                        logout
+                                .logoutUrl("/logout")
+                                .logoutSuccessHandler(customLogoutSuccessHandler)
+                                .invalidateHttpSession(true)
+                                .deleteCookies("JSESSIONID")
+                );
+
+        return http.build();
+    }
+}
