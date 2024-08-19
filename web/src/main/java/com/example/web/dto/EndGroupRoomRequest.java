74cbe42b1830828584ac99a904f2292005f31658
ByulKim <bbyyuull33@gmail.com>
2024-08-19 18:32:02 +0900

    request DTO 클래스 equals/hashcode 재정의

--- web/src/main/java/com/example/web/dto/EndGroupRoomRequest.java
+++ web/src/main/java/com/example/web/dto/EndGroupRoomRequest.java
@@ -1,14 +1,12 @@
 package com.example.web.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
+@EqualsAndHashCode
 public class EndGroupRoomRequest {
 
     @NotNull
